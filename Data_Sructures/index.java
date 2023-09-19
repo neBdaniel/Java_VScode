943f65db55af1df5cf4c22772243abbc32a4ae1c
BenDan <bendanrob2001@gmail.com>
2023-09-19 21:48:44 +0800

    fixed a typo

--- Data_Sructures/index.java
+++ Data_Sructures/index.java
@@ -14,7 +14,7 @@ public class index {
 
         System.out.println("DATA STRUCTURES THAT YOU NEED TO LEARN!!");
         System.out.println("1. ARRAYS");
-        System.out.println("2. STACK");
+        System.out.println("2. LINKED LIST");
         System.out.println("3. STACK");
         System.out.println("4. QUEUE");
         System.out.println("5. HASHMAPS/HASHTABLES");
